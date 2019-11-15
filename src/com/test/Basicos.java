150d5e84fa6f651809e03afe79d3326d5efa84ac
eneace13 <nievesaracil@gmail.com>
2019-11-15 11:45:29 +0100

    cadena

--- src/com/test/Basicos.java
+++ src/com/test/Basicos.java
@@ -1,5 +1,14 @@
 package com.test;
 
 public class Basicos {
-
+    public static void cadenas() {
+    	String cadena;
+    	
+    	cadena = "Hola Mundo";
+    	
+    	System.out.println(cadena);
+    }
+    public static void main(String[] args) {
+        cadenas(); 
+    }    
 }
