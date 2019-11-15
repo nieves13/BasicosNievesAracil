37b89fdbb51e0f293c8367756b7f43223f7e856d
eneace13 <nievesaracil@gmail.com>
2019-11-15 11:46:41 +0100

    logicos

--- src/com/test/Basicos.java
+++ src/com/test/Basicos.java
@@ -1,5 +1,20 @@
 package com.test;
 
 public class Basicos {
-
+	public static void logicos () {
+		boolean logico;
+		
+		logico = false;
+		
+		
+		if (logico == true) {
+			System.out.println("El resultado es verdadero.");
+		}
+		else {
+			System.out.println("El resultado es falso.");
+		}
+	}
+	public static void main(String[] args) {
+		logicos();
+	}
 }
